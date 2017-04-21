6bbd07d335f9ef4ef5ca079b9613873fda532cf7
Moritz Fischer <moritz.fischer@ettus.com>
2017-04-20 17:14:09 -0700

    mfd: ds1374: Add Dallas/Maxim DS1374 Multi Function Device
    Add support for the Maxim/Dallas DS1374 RTC/WDT with trickle charger.
    The device can either be configured as simple RTC, as simple RTC with
    Alarm (IRQ) as well as simple RTC with watchdog timer.
    
    Break up the old monolithic driver in drivers/rtc/rtc-ds1374.c into:
    - rtc part in drivers/rtc/rtc-ds1374.c
    - watchdog part under drivers/watchdog/ds1374-wdt.c
    - mfd part drivers/mfd/ds1374.c
    
    The MFD part takes care of trickle charging and mode selection,
    since the usage modes of a) RTC + Alarm or b) RTC + WDT
    are mutually exclusive.
    
    Signed-off-by: Moritz Fischer <mdf@kernel.org>

---
+++ include/dt-bindings/mfd/ds1374.h
@@ -0,0 +1,17 @@
+/*
+ * This header provides macros for Maxim/Dallas DS1374 DT bindings
+ *
+ * Copyright (C) 2017 National Instruments Corp
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ */
+
+#ifndef __DT_BINDINGS_MFD_DS1374_H__
+#define __DT_BINDINGS_MFD_DS1374_H__
+
+#define DALLAS_MODE_RTC		0
+#define DALLAS_MODE_ALM		1
+#define DALLAS_MODE_WDT		2
+
+#endif /* __DT_BINDINGS_MFD_DS1374_H__ */
